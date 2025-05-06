043ac5c3ae5ed1193cc13eb37dde4bed6e45b774
gooking <javazj@qq.com>
2025-05-06 15:54:43 +0800

    增加用户标签认证

--- index.js
+++ index.js
@@ -418,6 +418,9 @@ module.exports = {
   goodsCategoryDetail: (id) => {
     return request('/shop/goods/category/info', true, 'get', { id })
   },
+  goodsCategoryDetailV2: (data) => {
+    return request('/shop/goods/category/info', true, 'get', data)
+  },
   goodsv2: (data) => {
     if (!data) {
       data = {}
@@ -1433,6 +1436,15 @@ module.exports = {
   },
   idcardCheckManualReviewInfo: (token) => {
     return request('/user/idcard/manualReview/info', true, 'get', { token })
+  },
+  userTagList: (data) => {
+    return request('/userTag/list', true, 'post', data)
+  },
+  userTagCertificateQuery: (token) => {
+    return request('/userTag/certificate/query', true, 'get', { token })
+  },
+  userTagCertificate: (data) => {
+    return request('/userTag/certificate', true, 'post', data)
   },
   bindSeller: data => {
     return request('/user/bindSeller', true, 'post', data)
